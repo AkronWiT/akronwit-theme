0e56e3b9401bb6fb943f641f49f1e8b4245e53b1
Shannon Whalen <shannonwhalen@msn.com>
2016-03-23 22:34:59 -0400

    adding meetup api calls for member and event counts

--- page-home.php
+++ page-home.php
@@ -6,6 +6,29 @@ Template Name: Home
 
 <?php get_header(); ?>
 
+<!-- SMW adding meetup api counts to webpage -->
+<script>
+  $.ajax({
+	type:'GET',
+	url:'https://api.meetup.com/2/groups?offset=0&format=json&group_urlname=Akron-Women-In-Tech&only=members&callback=jQuery1113014036352676339447_1436571834493&photo-host=public&page=20&radius=25.0&fields=&order=id&desc=false&_=1436571834494&sig_id=45621862&sig=193eedf432bb623f60d8a47ece16bca89d4abcdd',
+	dataType: 'jsonp',
+	success: function(data, status, request){
+
+	  $('#members').html(data.results[0].members);
+	}
+  });
+
+  $.ajax({
+	type:'GET',
+	url:'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Akron-Women-In-Tech&photo-host=public&page=20&fields=&order=time&status=past%2Cupcoming&desc=false&sig_id=45621862&sig=994063bada2de80f9daac68a008e9ff5899f9b51',
+	dataType: 'jsonp',
+	success: function(data, status, request){
+	  
+	  $('#events').html(data.meta.total_count);
+	}
+  });
+</script>
+
 <!-- INTRODUCTION -->
 <div id="our-goal-wrapper" class="content-wrapper dark introduction">
   <div class="content">
@@ -30,8 +53,8 @@ Template Name: Home
 <div class="content-wrapper dark group-counts">
   <div class="content">
     <h2 class="title visually-hidden">Counts</h2>
-    <p class="count"><span class="number">194</span> Members</p>
-    <p class="count is-last"><span class="number">20</span> Meetups</p>
+    <p class="count"><span class="number" id="members">194</span> Members</p>
+    <p class="count is-last"><span class="number" id="events">20</span> Meetups</p>
   </div>
 </div>
 
